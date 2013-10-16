a2fd659608d0bdad570f5cd26fc3c59570514d74
Joseph Rhoads <joseph_rhoads@brown.edu>
2013-10-16 12:51:49 -0400

    Updated views to use render instead of render_to_response

--- views.py
+++ views.py
@@ -1,14 +1,14 @@
 """ Create your views here."""
-from django.shortcuts import render_to_response
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
-from django.template import RequestContext
-from django.http import HttpResponseRedirect
-from django.http import HttpResponse
+from django.http import (
+    HttpResponseRedirect,
+    HttpResponse,
+)
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from eulfedora.server import Repository
 
+from eulfedora.server import Repository
 from bdrcmodels.models import MasterImage
 from bdrcmodels.models import CommonMetadataDO
 
@@ -21,7 +21,9 @@ from .forms import (
     get_collections_choices,
     RepoLandingForm,
     FileReplacementForm,
+    EditXMLForm,
 )
+
 import requests
 import json
 
@@ -34,7 +36,7 @@ def landing(request):
         return HttpResponseRedirect(reverse("repo_direct:display", args=(pid,)))
     elif request.method == 'GET':
         form = RepoLandingForm()
-        return render_to_response('repo_direct/landing.html', {'form': form}, context_instance=RequestContext(request))
+        return render(request, 'repo_direct/landing.html', {'form': form})
 
 
 def display(request, pid):
@@ -53,7 +55,7 @@ def edit(request, pid):
             obj.save()
     elif request.method == 'GET':
         form = DublinCoreEditForm(instance=obj.dc.content)
-    return render_to_response('repo_direct/edit.html', {'form': form, 'obj': obj}, context_instance=RequestContext(request))
+    return render(request, 'repo_direct/edit.html', {'form': form, 'obj': obj})
 
 
 @login_required
@@ -66,7 +68,7 @@ def rights_edit(request, pid, dsid):
         obj = repo.get_object(pid, type=CommonMetadataDO)
         obj.rightsMD.content = new_rights
         obj.save()
-        messages.info(request, 'The sharing setting for %s have changed' % (pid,) )
+        messages.info(request, 'The sharing setting for %s have changed' % (pid,), extra_tags='text-info' )
         return HttpResponseRedirect(reverse("repo_direct:display", args=(pid,)))
     return render( request, 'repo_direct/rights_form.html', { 
         'form': form, 
@@ -78,28 +80,20 @@ def rights_edit(request, pid, dsid):
 def ir_edit(request, pid, dsid):
     repo = Repository()
     obj = repo.get_object(pid, type=CommonMetadataDO)
-    if request.method == "POST":
-        form = IrMetadataEditForm(request.POST)
-        form.fields['collections'].choices = get_collections_choices()
-        if form.is_valid():
-            new_collections = form.cleaned_data['collections']
-            obj = repo.get_object(pid, type=CommonMetadataDO)
-            obj.irMD.content.collection_list = new_collections
-            obj.save()
-            messages.info(request,
-                          'The collecitons for %s have been set to %s' % (pid, new_collections),
-                          extra_tags='text-info' )
-            
+    form = IrMetadataEditForm(request.POST or None)
+    form.fields['collections'].choices = get_collections_choices()
+    if form.is_valid():
+        new_collections = form.cleaned_data['collections']
+        obj = repo.get_object(pid, type=CommonMetadataDO)
+        obj.irMD.content.collection_list = new_collections
+        obj.save()
+        messages.info(request, 'The collecitons for %s have changed' % (pid,), extra_tags='text-info' )
         return HttpResponseRedirect(reverse("repo_direct:display", args=(pid,)))
-    elif request.method == 'GET':
-        form = IrMetadataEditForm()
-        form.fields['collections'].choices = get_collections_choices()
-        return render_to_response('repo_direct/edit.html',
-                                  {'form': form,
-                                   'obj': obj,
-                                   'dsid': "irMetadata"
-                                  },
-                                  context_instance=RequestContext(request))
+    return render(request, 'repo_direct/edit.html',{
+        'form': form, 
+        'obj': obj, 
+        'dsid': "irMetadata" 
+    })
 
 def file_edit(request, pid, dsid):
     repo = Repository()
@@ -120,12 +114,11 @@ def file_edit(request, pid, dsid):
         dsid="POSTED"
     elif request.method == 'GET':
         form = FileReplacementForm()
-    return render_to_response('repo_direct/file_edit.html',
+    return render(request, 'repo_direct/file_edit.html',
                               {'form': form,
                                'obj': obj,
                                'dsid': dsid
-                              },
-                              context_instance=RequestContext(request))
+                              })
 
 
 @login_required
@@ -141,18 +134,18 @@ def xml_edit(request, pid, dsid):
                 datastream_obj = obj.getDatastreamObject(dsid)
                 xml_content = u"%s" % form.cleaned_data['xml_content']
                 datastream_obj.content = xml_content.encode('utf-8')
-                #datastream_obj.content = form.cleaned_data['xml_content']
                 datastream_obj.save()
             obj.save()
             return HttpResponseRedirect(reverse("repo_direct:display", args=(pid,)))
     elif request.method == 'GET':
         if dsid in obj.ds_list:
             datastream_obj = obj.getDatastreamObject(dsid)
-            if dsid in ["MODS",]:
-                xml_content = datastream_obj.content
-            else:
-                xml_content = datastream_obj.content.serialize()
+            xml_content = datastream_obj.content.serialize(pretty=True)
         else:
             xml_content = "No datastream found"
         form = EditXMLForm({'xml_content': xml_content})
-    return render(request, 'repo_direct/edit2.html', {'form': form, 'obj': obj, 'dsid': dsid}, context_instance=RequestContext(request))
+    return render(request, 'repo_direct/edit2.html', {
+        'form': form, 
+        'obj': obj, 
+        'dsid': dsid
+    })
