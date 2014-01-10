d2a341da1bb9e219f8806b9277bffa479a74285b
Joseph Rhoads <joseph_rhoads@brown.edu>
2014-01-10 13:23:52 -0500

    Reworked base template extension

--- urls.py
+++ urls.py
@@ -5,7 +5,6 @@ from eulfedora.views import raw_datastream, raw_audit_trail
 urlpatterns = patterns(
     'repo_direct_app.views',
     url('^$', 'landing', name='landing'),
-    #url('^upload/$', 'upload', name='upload'),
     url(r'^(?P<pid>[^/]+)/$', 'display', name='display'),
     url(r'^(?P<pid>[^/]+)/(?P<dsid>(MODS|RELS-EXT|DC|METS|RELS-INT))/edit/$', 'xml_edit', name='xml-edit'),
     url(r'^(?P<pid>[^/]+)/(?P<dsid>(irMetadata))/edit/$', 'ir_edit', name='ir-edit'),
