21269ce59f963e057f9341fafcbd627ecdb3d7be
Joseph Rhoads <joseph_rhoads@brown.edu>
2015-11-10 14:15:23 -0500

    Annotate urls by using kwargs

--- urls.py
+++ urls.py
@@ -1,21 +1,61 @@
 from django.conf.urls import url, patterns
 from eulfedora.views import raw_datastream, raw_audit_trail
+from eulfedora import views as eulviews
+from . import views
 
 
 urlpatterns = patterns(
-    'repo_direct_app.views',
-    url('^$', 'landing', name='landing'),
-    url(r'^(?P<pid>[^/]+)/$', 'display', name='display'),
-    url(r'^(?P<pid>[^/]+)/(?P<dsid>(MODS|RELS-EXT|DC|METS|RELS-INT))/edit/$', 'xml_edit', name='xml-edit'),
-    url(r'^(?P<pid>[^/]+)/(?P<dsid>(irMetadata))/edit/$', 'ir_edit', name='ir-edit'),
-    url(r'^(?P<pid>[^/]+)/(?P<dsid>(rightsMetadata))/edit/$', 'rights_edit', name='rights-edit'),
-    url(r'^(?P<pid>[^/]+)/(?P<dsid>(tiff|jpg|JP2|PDF|content))/edit/$', 'file_edit', name='file-edit'),
+    '',
+    url(
+        regex= r'^$',
+        view = views.landing,
+        name = 'landing'
+    ),
+    url(
+        regex= r'^(?P<pid>[^/]+)/$',
+        view = views.display,
+        name = 'display'
+    ),
+    url(
+        regex= r'^(?P<pid>[^/]+)/(?P<dsid>(MODS|RELS-EXT|DC|METS|RELS-INT))/edit/$',
+        view = views.xml_edit,
+        name = 'xml-edit'
+    ),
+    url(
+        regex= r'^(?P<pid>[^/]+)/(?P<dsid>(irMetadata))/edit/$',
+        view = views.ir_edit,
+        name = 'ir-edit'
+    ),
+    url(
+        regex= r'^(?P<pid>[^/]+)/(?P<dsid>(rightsMetadata))/edit/$',
+        view = views.rights_edit,
+        name = 'rights-edit'
+    ),
+    url(
+        regex= r'^(?P<pid>[^/]+)/(?P<dsid>(tiff|jpg|JP2|PDF|content))/edit/$',
+        view = views.file_edit,
+        name = 'file-edit'
+    ),
 )
 urlpatterns += patterns(
     '',
-    url(r'^(?P<pid>[^/]+)/AUDIT/$', raw_audit_trail),
-    url(r'^(?P<pid>[^/]+)/(?P<dsid>(MODS|RELS-EXT|DC|METS|RELS-INT))/$', raw_datastream),
-    url(r'^(?P<pid>[^/]+)/(?P<dsid>(irMetadata|rightsMetadata))/$', raw_datastream),
-    url(r'^(?P<pid>[^/]+)/(?P<dsid>(tiff|jpg|JP2|PDF|content))/$', raw_datastream),
+    url(
+        regex= r'^(?P<pid>[^/]+)/AUDIT/$',
+        view = eulviews.raw_audit_trail
+        name = 'audit'
+    ),
+    url(
+        regex= r'^(?P<pid>[^/]+)/(?P<dsid>(MODS|RELS-EXT|DC|METS|RELS-INT))/$',
+        view = eulviews.raw_datastream,
+        name = 'raw-datastream'
+    ),
+    url(
+        regex= r'^(?P<pid>[^/]+)/(?P<dsid>(irMetadata|rightsMetadata))/$',
+        view = eulviews.raw_datastream
+    ),
+    url(
+        regex= r'^(?P<pid>[^/]+)/(?P<dsid>(tiff|jpg|JP2|PDF|content))/$',
+        view = eulviews.raw_datastream
+    ),
 
 )
